9d2bdaeb20b601f3bdbd6cbeb134cccffced5216
sheetal-10 <sheetalp257@gmail.com>
2021-08-30 23:27:28 +0530

    Added: test case to return happy

--- src/com/bridgelabzs/MoodAnalyserTest.java
+++ src/com/bridgelabzs/MoodAnalyserTest.java
@@ -10,11 +10,19 @@ import org.junit.Test;
  * @since 2021-08-29
  */
 public class MoodAnalyserTest {
-	//Test case for Sad Mood
+		//Test case for Sad Mood
 		@Test
 		public void givenMessgae_WhenSad_ShouldReturn_Sad() {
 			MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
 			String mood = moodAnalyser.analyseMood("I am in Sad Mood");
 			Assert.assertEquals("SAD", mood);
+		}
+		
+		//Test case for Happy Mood
+		@Test
+		public void givenMessage_whenNotSad_shouldReturn_Happy() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
+			String mood = moodAnalyser.analyseMood("I am in any Mood");
+			Assert.assertEquals("HAPPY", mood);
 		}
 }
