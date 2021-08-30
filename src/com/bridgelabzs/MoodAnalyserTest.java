4f6742affdb0e86243fad90d96e7ba21e876e97c
sheetal-10 <sheetalp257@gmail.com>
2021-08-30 23:22:40 +0530

    Added: Test case to return sad

---
+++ src/com/bridgelabzs/MoodAnalyserTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabzs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * JUnit Test case for validating the return statement
+ * Validating with Multiple run cases
+ * @author Sheetal
+ * @since 2021-08-29
+ */
+public class MoodAnalyserTest {
+	//Test case for Sad Mood
+		@Test
+		public void givenMessgae_WhenSad_ShouldReturn_Sad() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
+			String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+			Assert.assertEquals("SAD", mood);
+		}
+}
